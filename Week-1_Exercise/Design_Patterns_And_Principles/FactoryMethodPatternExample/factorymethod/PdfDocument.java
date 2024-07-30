e659f78ccb5427ab29bf7aaeee6dc32608dd09e4
SURAPANENI-SURYA-MOUNIKA <117089104+SURAPANENI-SURYA-MOUNIKA@users.noreply.github.com>
2024-07-30 20:03:14 +0530

    Add files via upload

---
+++ Week-1_Exercise/Design_Patterns_And_Principles/FactoryMethodPatternExample/factorymethod/PdfDocument.java
@@ -0,0 +1,18 @@
+package factorymethod;
+import java.io.File;
+public class PdfDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening PDF document...");
+    }
+
+    @Override
+    public void save() {
+        System.out.println("Saving PDF document...");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closing PDF document...");
+    }
+}
